ffea3cb1660f411410376e1e12faef2635d5d5dd
朱克冉 <1343246304@qq.com>
2026-04-10 16:16:17 +0800

    update label name

--- src/components/layout/ActivityBar.vue
+++ src/components/layout/ActivityBar.vue
@@ -34,7 +34,7 @@ const emit = defineEmits<{
         <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
         <polyline points="14 2 14 8 20 8"></polyline>
       </svg>
-      <span>Logs</span>
+      <span>Terminal</span>
     </div>
   </nav>
 </template>
